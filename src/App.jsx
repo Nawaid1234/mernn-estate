7640967e79e5d104b3bccabd02c4f7ce7780a110
Nawaid Iqbal <iqbalnawaid15@gmail.com>
2025-05-19 02:56:33 +0530

    added the header components and the updated the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
+
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="bg-white p-6 rounded-lg shadow-md text-center">
-          <h1 className="text-2xl font-bold text-blue-600 mb-4">Real Estate Project</h1>
-          <p className="text-gray-700">Tailwind CSS is working perfectly!</p>
-        </div>
-      </div>
+      <BrowserRouter>
+
+      <Header />
+        {/* Tailwind wala div Routes ke bahar hona chahiye
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <h1 className="text-3xl font-bold text-blue-600">Tailwind is working!</h1>
+        </div> */}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-} 
+  );
+}
 
 export default App;
